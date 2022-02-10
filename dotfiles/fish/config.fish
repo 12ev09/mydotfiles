1062067ed6720fd061f2ea444c4d2d02010e2752
Takumi <t-nakayama-1w1@eagle.sophia.ac.jp>
2022-02-10 22:59:13 +0900

    add fish settings

---
+++ dotfiles/fish/config.fish
@@ -0,0 +1,9 @@
+#view
+set -g theme_display_date yes
+set -g theme_date_format "+%F %H:%M"
+set -g theme_display_git_default_branch yes
+set -g theme_color_scheme dark
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
